b047ebbc757909459d29668c31ed44eee73f1761
Alba <albagar4@student.42malaga.com>
2024-08-06 13:55:20 +0200

    added: ex00 module_01

---
+++ module_01/ex00/main.cpp
@@ -0,0 +1,18 @@
+#include "Zombie.hpp"
+
+// - zombie's constructor and destructor are automatically called
+// - newZombie comes from a function that returns a pointer to the Zombie Class
+// and uses new and delete to allocate and unallocate it correctly
+// - randomChump is a function that creates, announces and destroys the Zombie all by itself
+
+int	main(void){
+	Zombie	zombie("Arthur");
+	Zombie	*_newZombie;
+
+	_newZombie = newZombie("Tadeo");
+	randomChump("Thomas");
+	zombie.announce();
+	_newZombie->announce();
+	delete _newZombie;
+	return (0);
+}

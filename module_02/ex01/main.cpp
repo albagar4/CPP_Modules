b5c55d2662a8b833d9661a97d2b2825ed624fd72
Alba <albagar4@student.42malaga.com>
2024-08-26 11:44:17 +0200

    added: another main to ex01

--- module_02/ex01/main.cpp
+++ module_02/ex01/main.cpp
@@ -1,23 +1,35 @@
 #include "Fixed.hpp"
 
-int main( void ) {
+int	main(void){
+	Fixed a(5);
+	Fixed b(3.75f);
 
-Fixed a;
-Fixed const b(10);
-Fixed const c(42.42f);
-Fixed const d(b);
+	std::cout << BRED << "a from rawBits: " << a.getRawBits() << RNL;
+	std::cout << BRED << "b from rawBits: " << b.getRawBits() << RNL;
 
-a = Fixed(1234.4321f);
-
-std::cout << "a is " << a << std::endl;
-std::cout << "b is " << b << std::endl;
-std::cout << "c is " << c << std::endl;
-std::cout << "d is " << d << std::endl;
-
-std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-std::cout << "d is " << d.toInt() << " as integer" << std::endl;
-
-return (0);
+	std::cout << CYAN << "a from toInt: " << a.toInt() << RNL;
+	std::cout << CYAN << "b from toInt: " << b.toInt() << RNL;
+	std::cout << CYAN << "b from toFloat: " << b.toFloat() << RNL;
 }
+
+// int main( void ) {
+
+// Fixed a;
+// Fixed const b(10);
+// Fixed const c(42.42f);
+// Fixed const d(b);
+
+// a = Fixed(1234.4321f);
+
+// std::cout << "a is " << a << std::endl;
+// std::cout << "b is " << b << std::endl;
+// std::cout << "c is " << c << std::endl;
+// std::cout << "d is " << d << std::endl;
+
+// std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+// std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+// std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+// std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+
+// return (0);
+// }

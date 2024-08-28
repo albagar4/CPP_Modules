b50e448e0414a4a4feace3ae3e4b50395dd23770
Alba <albagar4@student.42malaga.com>
2024-08-28 13:24:57 +0200

    started: module 03 ex00

--- module_03/ex00/ClapTrap.cpp
+++ module_03/ex00/ClapTrap.cpp
@@ -1,22 +1,49 @@
 #include "./ClapTrap.hpp"
 
-ClapTrap::ClapTrap()
-{
-	// std::cout << "ClapTrap: Default constructor called" << std::endl;
+ClapTrap::ClapTrap(){
+	std::cout << CYAN << "ClapTrap: Default constructor called" << RNL;
+	this->name = "Default";
+	this->hitPoints = 10;
+	this->energyPoints = 10;
+	this->attackDamage = 0;
 }
 
-ClapTrap::~ClapTrap()
-{
-	// std::cout << "ClapTrap: Destructor called" << std::endl;
+ClapTrap::ClapTrap(std::string name){
+	std::cout << CYAN << "ClapTrap: " << name << " constructor called" << RNL;
+	this->name = name;
+	hitPoints = 10;
+	energyPoints = 10;
+	attackDamage = 0;
 }
 
-ClapTrap::ClapTrap(const ClapTrap &claptrap)
-{
-	// std::cout << "ClapTrap: Copy constructor called" << std::endl;
+ClapTrap::~ClapTrap(){
+	std::cout << CYAN << "ClapTrap: " << name << " Destructor called" << RNL;
 }
 
-ClapTrap &ClapTrap::operator=(const ClapTrap &claptrap)
-{
-	// std::cout << "ClapTrap: Copy assignment operator called" << std::endl;
+ClapTrap::ClapTrap(const ClapTrap &claptrap){
+	std::cout << CYAN << "ClapTrap: Copy constructor called" << RNL;
+	this->name = claptrap.name;
+	this->hitPoints = claptrap.hitPoints;
+	this->energyPoints = claptrap.energyPoints;
+	this->attackDamage = claptrap.attackDamage;
+}
+
+ClapTrap &ClapTrap::operator=(const ClapTrap &claptrap){
+	std::cout << CYAN << "ClapTrap: Copy assignment operator called" << RNL;
+	this->name = claptrap.name;
+	this->hitPoints = claptrap.hitPoints;
+	this->energyPoints = claptrap.energyPoints;
+	this->attackDamage = claptrap.attackDamage;
 	return (*this);
+}
+
+void ClapTrap::attack(const std::string& target){
+	if (this->energyPoints > 0)
+	{
+		std::cout << this->name << " has attacked " << target << "!!" << std::endl;
+		std::cout << target << " has received " << this->attackDamage << " points of damage" << std::endl; 
+		this->energyPoints--;
+	}
+	else
+		std::cout << this->name << " has no Energy points left" << std::endl;
 }
--- module_03/ex00/ClapTrap.hpp
+++ module_03/ex00/ClapTrap.hpp
@@ -1,18 +1,38 @@
 #ifndef CLAPTRAP_HPP
 # define CLAPTRAP_HPP
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <stdlib.h>
 
-# include <iostream>
+#define RED    "\033[0;31m"
+#define BRED    "\033[1;31m"
+#define CYAN    "\033[1;36m"
+#define RESET    "\033[0m"
+#define RNL    "\033[0m\n"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define GRAY "\033[1;30m"
+#define PURPLE "\033[35m"
+#define LGRAY "\033[0;37m"
+#define BOLD "\033[1;37m"
 
-class ClapTrap
-{
-  public:
+class ClapTrap{
+private:
+	std::string name;
+	unsigned int hitPoints;
+	unsigned int energyPoints;
+	unsigned int attackDamage;
+public:
 	ClapTrap();
+	ClapTrap(std::string name);
 	ClapTrap(const ClapTrap &claptrap);
 	ClapTrap &operator=(const ClapTrap &claptrap);
 	~ClapTrap();
-
-  private:
-	
+	void attack(const std::string& target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
 };
 
 #endif

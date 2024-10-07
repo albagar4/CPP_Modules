cb813669698bee5db5feb2f33e14b95ca3b7771c
Alba <albagar4@student.42malaga.com>
2024-10-07 15:19:00 +0200

    changed: main and makefile ex03

--- module_05/ex03/main.cpp
+++ module_05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: albagar4 <albagar4@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 18:35:38 by albagar4          #+#    #+#             */
-/*   Updated: 2024/10/07 14:32:04 by albagar4         ###   ########.fr       */
+/*   Updated: 2024/10/07 15:17:59 by albagar4         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,13 @@ int main(void){
 	AForm* rrf;
 	Bureaucrat bury("Steve", 1);
 	
-	rrf = someRandomIntern.makeForm("shrubbery creation", "Bender");
-	std::cout << rrf->getName() << std::endl;
-	bury.signForm(*rrf);
-	bury.executeForm(*rrf);
+	rrf = someRandomIntern.makeForm("president request", "Bender");
+	if (rrf)
+	{
+		std::cout << rrf->getName() << std::endl;
+		bury.signForm(*rrf);
+		bury.executeForm(*rrf);
+	}
 	}
 	return (0);
 }

41762bc8b1a51954214c00bace00df0f40a1a6f6
albagar4 <alba.gar.diaz@gmail.com>
2024-10-11 09:48:56 +0200

    testing: ex02 CPP 07

--- module_07/ex02/Array.hpp
+++ module_07/ex02/Array.hpp
@@ -22,10 +22,11 @@
 #define LGRAY "\033[0;37m"
 #define BOLD "\033[1;37m"
 
-template <typename T>
+template <class T>
 class Array {
 private:
 	T *array;
+	int arraySize;
 public:
 	Array();
 	Array(unsigned int n);
@@ -34,8 +35,69 @@ public:
 	~Array();
 	int size() const;
 	T &operator[](int index) const;
+	class OutOfLimits: public std::exception {
+		const char *what() const throw();
+	};
 };
 
-#include "Array.tpp"
+template <class T>
+Array<T>::Array() {
+	this->array = NULL;
+	this->arraySize = 0;
+	std::cout << "Array: Default constructor called" << std::endl;
+}
+
+template <class T>
+Array<T>::Array(unsigned int n) {
+	this->array = new T[n];
+	this->arraySize = n;
+	std::cout << "Array: Parameterized constructor called" << std::endl;
+}
+
+template <class T>
+Array<T>::Array(const Array &array) {
+	T *cpy = new T[array.arraySize];
+	for (int i; i < array.arraySize; i++)
+		cpy[i] = array[i];
+	this->array = cpy;
+	this->arraySize = array.arraySize;
+	std::cout << "Array: Copy constructor called" << std::endl;
+}
+
+template <class T>
+Array<T> &Array<T>::operator=(const Array &array) {
+	if (this->array)
+		delete [] this->array;
+	T *cpy = new T[array.arraySize];
+	for (int i; i < array.arraySize; i++)
+		cpy[i] = array[i];
+	this->array = cpy;
+	this->arraySize = array.arraySize;
+	std::cout << "Array: Copy operator constructor called" << std::endl;
+	return (*this);
+}
+
+template <class T>
+Array<T>::~Array() {
+	if (this->array)
+		delete [] this->array;
+	std::cout << "Array: Destructor called" << std::endl;
+}
+
+template <class T>
+int Array<T>::size() const {return (this->arraySize);}
+
+template <class T>
+T &Array<T>::operator[](int index) const {
+	if (index < 0 || index >= this->arraySize)
+		throw OutOfLimits();
+	// std::cout << "I'm returning: " << this->array[index] << std::endl;
+	return (this->array[index]);
+}
+
+template <class T>
+const char *Array<T>::OutOfLimits::what() const throw() {
+	return ("The index you're introducing is not correct");
+}
 
 #endif

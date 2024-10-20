89d1e0908cf3a09e1c4fcc1230ae1d1ddf4a3466
albagar4 <alba.gar.diaz@gmail.com>
2024-10-20 16:13:20 +0200

    ex01: done

--- module_09/ex01/RPN.cpp
+++ module_09/ex01/RPN.cpp
@@ -21,8 +21,8 @@ int evaluateRPN(char *argv) {
 	std::stack<float> nbrs;
 	float result = 0;
 
-	if (strlen(argv) == 1 && isdigit(argv[0])) {
-		std::cout << argv[0] << std::endl;
+	if ((strlen(argv) == 1 && isdigit(argv[0])) || (strlen(argv) == 2 && argv[0] == '-' && isdigit(argv[1]))) {
+		std::cout << argv << std::endl;
 		return (0);
 	}
 	for (long unsigned int i = 0; i < strlen(argv); i++) {
@@ -40,19 +40,27 @@ int evaluateRPN(char *argv) {
 		}
 		else if (argv[i] == '+' || argv[i] == '-' || argv[i] == '*' || argv[i] == '/') {
 			if (nbrs.empty() || nbrs.size() < 2 || (argv[i + 1] != ' ' && argv[i + 1] != '	' && argv[i + 1] != '\0')) {
-				std::cout << "Error: bad format. Operators must be placed after the values and followed by space or tab" << std::endl;
-				return (-1);
+				if (argv[i] == '-' && isdigit(argv[i + 1]) && (argv[i + 2] == ' ' || argv[i + 2] == '	')) {
+					nbrs.push(atof(&argv[i + 1]) * (-1));
+					i++;
+				}
+				else {
+					std::cout << "Error: bad format. Operators must be placed after the values and followed by space or tab" << std::endl;
+					return (-1);
+				}
 			}
-			float second = nbrs.top();
-			nbrs.pop();
-			float first = nbrs.top();
-			nbrs.pop();
-			if (argv[i] == '/' && (first == 0 || second == 0)) {
-				std::cout << "Error: divisions by 0 are not allowed" << std::endl;
-				return (-1);
+			else {
+				float second = nbrs.top();
+				nbrs.pop();
+				float first = nbrs.top();
+				nbrs.pop();
+				if (argv[i] == '/' && (first == 0 || second == 0)) {
+					std::cout << "Error: divisions by 0 are not allowed" << std::endl;
+					return (-1);
+				}
+				result = evaluatePhase(first, second, argv[i]);
+				nbrs.push(result);
 			}
-			result = evaluatePhase(first, second, argv[i]);
-			nbrs.push(result);
 		}
 	}
 	if (nbrs.size() != 1) {
